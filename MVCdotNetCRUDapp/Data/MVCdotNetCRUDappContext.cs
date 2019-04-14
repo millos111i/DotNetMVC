cca17e8bcc9b44b3c5647e18497bec504352945b
Kamil Wlazło <kamil119988@wp.pl>
2019-04-14 20:36:00 +0200

    Model

---
+++ MVCdotNetCRUDapp/Data/MVCdotNetCRUDappContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVCdotNetCRUDapp.Models
+{
+    public class MVCdotNetCRUDappContext : DbContext
+    {
+        public MVCdotNetCRUDappContext (DbContextOptions<MVCdotNetCRUDappContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVCdotNetCRUDapp.Models.Movie> Movie { get; set; }
+    }
+}
--- MVCdotNetCRUDapp/Startup.cs
+++ MVCdotNetCRUDapp/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using MVCdotNetCRUDapp.Models;
 
 namespace MVCdotNetCRUDapp
 {
@@ -33,6 +35,9 @@ namespace MVCdotNetCRUDapp
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<MVCdotNetCRUDappContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("MVCdotNetCRUDappContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
