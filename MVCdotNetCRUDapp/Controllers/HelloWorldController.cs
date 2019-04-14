82f6779550a51cc2615ee9db6aec9c8eae6f4a25
Kamil Wlazło <kamil119988@wp.pl>
2019-04-14 18:51:14 +0200

    Controller update

---
+++ MVCdotNetCRUDapp/Controllers/HelloWorldController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MVCdotNetCRUDapp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //GET: /HelloWorld/
+        public string Index(string name, int id = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {id}");
+        }
+        //GET: /HelloWorld/Welcome
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+    }
+}
