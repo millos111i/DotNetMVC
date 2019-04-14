c15529665022de9cf6ef728823eced9af8a2ebb2
Kamil Wlazło <kamil119988@wp.pl>
2019-04-14 20:59:07 +0200

    DB Seed

--- MVCdotNetCRUDapp/Data/MVCdotNetCRUDappContext.cs
+++ MVCdotNetCRUDapp/Data/MVCdotNetCRUDappContext.cs
@@ -8,9 +8,16 @@ namespace MVCdotNetCRUDapp.Models
 {
     public class MVCdotNetCRUDappContext : DbContext
     {
+        private Func<Type, object> getRequiredService;
+
         public MVCdotNetCRUDappContext (DbContextOptions<MVCdotNetCRUDappContext> options)
             : base(options)
         {
+        }
+
+        public MVCdotNetCRUDappContext(Func<Type, object> getRequiredService)
+        {
+            this.getRequiredService = getRequiredService;
         }
 
         public DbSet<MVCdotNetCRUDapp.Models.Movie> Movie { get; set; }
--- MVCdotNetCRUDapp/Program.cs
+++ MVCdotNetCRUDapp/Program.cs
@@ -5,8 +5,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using MVCdotNetCRUDapp.Models;
 
 namespace MVCdotNetCRUDapp
 {
@@ -14,7 +17,24 @@ namespace MVCdotNetCRUDapp
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+            using(var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<MVCdotNetCRUDappContext>();
+                    context.Database.Migrate();
+                    SeedData.Initialize(services);
+                }
+                catch(Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occured seeding the DB");
+                }
+            }
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
