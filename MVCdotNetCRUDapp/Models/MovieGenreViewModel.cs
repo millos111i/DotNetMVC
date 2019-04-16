ba4b69618cf69eb47be48b3699002a4ec5fe522c
Kamil Wlazło <kamil119988@wp.pl>
2019-04-16 21:17:00 +0200

    Search bar added

---
+++ MVCdotNetCRUDapp/Models/MovieGenreViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace MVCdotNetCRUDapp.Models
+{
+    public class MovieGenreViewModel
+    {
+        public List<Movie> Movies;
+        public SelectList Genres;
+        public string MovieGenre { get; set; }
+        public string SearchString { get; set; }
+    }
+}
