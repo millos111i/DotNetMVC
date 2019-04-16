577a19f698dbdea532cda2b2a75cb3ba25644a88
Kamil Wlazło <kamil119988@wp.pl>
2019-04-16 21:35:18 +0200

    Model validation added

--- MVCdotNetCRUDapp/Models/Movie.cs
+++ MVCdotNetCRUDapp/Models/Movie.cs
@@ -7,14 +7,21 @@ namespace MVCdotNetCRUDapp.Models
     public class Movie
     {
         public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
         public string Title { get; set; }
-        
-        [Display(Name ="Release Date")]
-        [DataType(DataType.Date)]
+
+        [Display(Name = "Release Date"), DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
+        [StringLength(30), Required, RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
         public string Genre { get; set; }
 
-        [Column(TypeName ="decimal(18, 2)")]
+        [DataType(DataType.Currency), Range(1, 100)]
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+
+        [Required, StringLength(5), RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        public string Rating { get; set; }
     }
 }
